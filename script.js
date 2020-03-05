33227aa21a9db6986a8194d59fae660984ca8f9c
dmitriy denisov <dmitriyd2009@yandex.ru>
2020-03-05 22:48:44 +0300

    day 2

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const data = {
+    currentCardNumber: 4,
+    question2: null,
+    question3: [],
+    question4: null,
+    question5: {
+        name: '',
+        email: '',
+        confirm: false
+    }
+}
+ShowCard (data.currentCardNumber)
+
+document
+    .querySelector('[data-start]')
+    .addEventListener('click', () => {
+        data.currentCardNumber = 2
+        ShowCard(2)
+    })
+
+document
+    .querySelector('[data-back]')
+    .addEventListener('click', () => {
+        --data.currentCardNumber
+        ShowCard(data.currentCardNumber)
+    })
+
+document
+    .querySelector('[data-next]')
+    .addEventListener('click', () => {
+        data.currentCardNumber++
+        ShowCard(data.currentCardNumber)
+    })
+
+document
+    .querySelector('[data-card="2"]')
+    .addEventListener('click', event => {
+        const liElement = event.target.closest('li')
+
+        if(!liElement) {
+            return
+        }
+
+        const inputElement = liElement.querySelector('input')
+        data.question2 = inputElement.value
+
+        ShowCard(data.currentCardNumber)
+    })
+
+document
+    .querySelector('[data-card="4"]')
+    .addEventListener('click', event => {
+        const liElement = event.target.closest('li')
+
+        if(!liElement) {
+            return
+        }
+
+        const inputElement = liElement.querySelector('input')
+        data.question4 = inputElement.value
+
+        ShowCard(data.currentCardNumber)
+    })    
+
 function ShowCard (n){
     if(n === 1 || n === 6){      
         hideFooter()
@@ -11,7 +75,41 @@ function ShowCard (n){
         .querySelectorAll('[data-card]')
         .forEach(x => x.style.display = 'none')
 
-        document.querySelector(`[data-card="${n}"]`).style.display = ''
+    const cardElement = document.querySelector(`[data-card="${n}"]`)
+    cardElement.style.display = ''
+
+    const nextButton = document.querySelector('[data-next]')
+    nextButton.setAttribute('disabled', true)
+
+    if( n === 2) {
+        cardElement
+            .querySelectorAll('input')
+            .forEach(inputElement => {
+                inputElement.removeAttribute('checked')
+
+                if ( inputElement.value === data.question2) {
+                    inputElement.setAttribute('checked', true)
+                }
+            })
+        if(data.question2) {
+            nextButton.removeAttribute('disabled')
+        }
+    }
+
+    if( n === 4) {
+        cardElement
+            .querySelectorAll('input')
+            .forEach(inputElement => {
+                inputElement.removeAttribute('checked')
+
+                if ( inputElement.value === data.question4) {
+                    inputElement.setAttribute('checked', true)
+                }
+            })
+        if(data.question4) {
+            nextButton.removeAttribute('disabled')
+        }
+    }
 }
 
 function showFooter() {
